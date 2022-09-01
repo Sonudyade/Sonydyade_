125c31c1d20b285980bdf843fd552361bac70351
Sonudyade <sonudyade46@gmail.com>
2022-09-01 20:24:23 +0530

    college.java

---
+++ college.java
@@ -0,0 +1,47 @@
+class Markscard { 
+       public static void main ( String [] arge ) {
+
+               char [] branch = {'c','i','v','i','l'};
+               System.out.println(branch [0] );
+               System.out.println(branch [1] );
+               System.out.println(branch [2] );
+               System.out.println(branch [3] );
+               System.out.println(branch [4] );
+               
+               float [] marks = {88.5f, 78.9f, 82.2f, 79.3f, 91.2f};
+               System.out.println(marks);
+               System.out.println( marks [4] );
+               
+               int [] date = {'2','6','0','8','2','0','2','2'};
+               System.out.println(date);
+               System.out.println(date[0] +""+ date[4]);
+               
+               long [] adharNumber = {'8', '3', '3', '3', '2', '4', '4', '6', '1', '5', '3', '8'};
+               System.out.println(adharNumber);
+               System.out.println( adharNumber [5] );
+               
+               short [] usn = {'3', '2'};
+               System.out.println( usn);  
+               System.out.println( usn [0] );  
+               
+               boolean [] pass = {true,false,true,false};
+               System.out.println(pass [0] +""+ pass[1] +""+ pass[2] +""+ pass[3]);
+               
+               byte [] sem = { '8' };
+               System.out.println(sem );
+               
+               double [] collegecode = {'1', '2', '3', '4', '5', '6'};
+               System.out.println(collegecode );
+               System.out.println( collegecode [5] );
+             }
+}
+
+
+
+
+
+
+               
+        
+
+          
