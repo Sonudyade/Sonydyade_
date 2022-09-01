96353e42f9c9463644de7692487c44b110e92f21
Sonudyade <sonudyade46@gmail.com>
2022-09-01 20:20:28 +0530

    string.java

---
+++ string.java
@@ -0,0 +1,16 @@
+class Style {
+        public static void main (String[]arge){
+        String type1 = "Beautiful girl";
+        String type2 = "Girl";
+
+          System.out.println(type1.toCharArray());
+          System.out.println(type1.contains("Girl"));
+          System.out.println(type1.equals(type2));
+          System.out.println(type1.replace("Beautiful","c++"));
+          System.out.println(type1.valueOf(type1));
+          System.out.println(type2.valueOf(type2));
+          System.out.println(type1.split("d",4));
+         
+          }
+}
+          
