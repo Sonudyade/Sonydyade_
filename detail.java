cc3e9454587c0495efff26f6ca7fb58aac4492a1
Sonudyade <sonudyade46@gmail.com>
2022-09-01 20:30:31 +0530

    detail.java

---
+++ detail.java
@@ -0,0 +1,29 @@
+class Bike{
+     public static void main(String [] pulsar){
+            System.out.println("The bike is Bajaj Pulsar");
+
+               byte petrol=20;
+               System.out.println(petrol);
+
+               short distriCode=32;
+               System.out.println(distriCode);
+
+               int bikeNumber =6789;
+               System.out.println(bikeNumber);
+
+               long licenceNo =5784396;
+               System.out.println(licenceNo);
+ 
+               float cgpa = 88 ;
+               System.out.println(cgpa);
+
+               double ab =1484;
+               System.out.println(ab);
+
+               char firstName='S';
+               System.out.println(firstName);
+
+               boolean costly=true;
+               System.out.println(costly);
+            }
+}
