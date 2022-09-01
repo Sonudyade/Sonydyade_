6845530575e498bf5bc20d612859d7afa88cdbd7
Sonudyade <sonudyade46@gmail.com>
2022-09-01 20:09:16 +0530

    Humanxyz.java

---
+++ Humanxyz.java
@@ -0,0 +1,16 @@
+class Humanxyz {
+    public static void main (String []arge) {
+        Human s = new Human();
+
+  System.out.println(s.Name);
+  System.out.println(s.color);
+  System.out.println(s.legs);
+  System.out.println(s.hands);
+  System.out.println(s.eyes);
+  System.out.println(s.Stay);
+  System.out.println(s.brain);
+  System.out.println(s.Gender);
+  System.out.println(s.Hairs);
+  System.out.println(s.Heart);
+}
+  }
