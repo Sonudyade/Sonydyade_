679fc88308fd41489b5da6fb4df4c0d399904a1f
Sonudyade <sonudyade46@gmail.com>
2022-09-01 20:27:55 +0530

    sonu.java

---
+++ sonu.java
@@ -0,0 +1,24 @@
+class college{
+      
+
+      static long moNO;
+      static boolean student;
+      static char firstName;
+      static byte age ;
+      static float cgpa;
+      static double percentage;
+      static int pageNumber;
+      static short book;
+
+      public static void main (String [] arge){
+
+             System.out.println("Default value  for long is:"+ moNO);
+             System.out.println( "Default value for boolean is:"+ student);
+             System.out.println( "Default value for char is:"+ firstName );
+             System.out.println( "Default value for byte is:"+ age);  
+             System.out.println( "Default value for float is:"+ cgpa);
+             System.out.println("Default value for double is:"+ percentage);
+             System.out.println("Default value for int is:"+ pageNumber);
+             System.out.println("Default value for short is:"+ book);
+           }
+}
