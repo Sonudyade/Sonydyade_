7ae5b63f67c6c092a5a908790914b2d788c1b5a0
Sonudyade <sonudyade46@gmail.com>
2022-09-02 20:34:45 +0530

    Studentxyz.java

---
+++ Studentxyz.java
@@ -0,0 +1,29 @@
+class Studentxyz {
+     public static void main (String []arge){
+         Student student = new Student();
+
+
+          System.out.println(student);
+          System.out.println(student.age);
+          System.out.println(student.firstName);
+          System.out.println(student.moNo);
+          System.out.println(student.gender);
+          System.out.println(student.color);
+
+          Student student1 = new Student();
+
+          System.out.println(student1);
+          System.out.println(student1.age);
+          System.out.println(student1.firstName);
+          System.out.println(student1.moNo);
+          System.out.println(student1.gender);
+          System.out.println(student1.color);
+   
+          Studentxyz Studentxyz = new Studentxyz();
+}
+     
+          Studentxyz(){
+               System.out.println("creating object of Studentxyz");
+
+     }
+  }
