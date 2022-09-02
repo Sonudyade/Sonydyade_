46a7f1419c19be1becd207e94630556b44245ce7
Sonudyade <sonudyade46@gmail.com>
2022-09-02 22:48:40 +0530

    Mobilevivo.java

---
+++ Mobilevivo.java
@@ -0,0 +1,31 @@
+class Mobilevivo {
+      public static void main (String [] arge){
+          Mobile mobile = new Mobile();
+
+      System.out.println(mobile);
+      System.out.println(mobile.name);
+      System.out.println(mobile.model);
+      System.out.println(mobile.color);
+      System.out.println(mobile.cost);
+      System.out.println(mobile.quality);
+
+      Mobile mobile1 = new Mobile();
+
+      System.out.println(mobile1);
+      System.out.println(mobile1.name);
+      System.out.println(mobile1.model);
+      System.out.println(mobile1.color);
+      System.out.println(mobile1.cost);
+      System.out.println(mobile1.quality);
+
+    Mobilevivo Mobilevivo = new Mobilevivo();
+}
+
+    Mobilevivo(){
+        System.out.println("creating object of Mobilevivo");
+
+   }
+}
+      
+      
+ 
