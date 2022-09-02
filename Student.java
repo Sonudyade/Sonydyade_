b0f3c66939702648b8e0a8fea002aac4995fc243
Sonudyade <sonudyade46@gmail.com>
2022-09-02 22:57:23 +0530

    Student.java

---
+++ Student.java
@@ -0,0 +1,18 @@
+class Student {
+    byte age;
+    String firstName;
+    long moNo;
+    String gender;
+    String color;
+   
+
+      Student () {
+        System.out.println("creating object");
+        age = 23;
+        firstName = "Sony";
+        moNo = 1;
+        gender = "Female";
+        color = " White ";
+        System.out.println("object created");
+}
+     }
