97bec924d0a68426d6b46cfcafffb6378475db8d
Sonudyade <sonudyade46@gmail.com>
2022-09-06 23:18:02 +0530

    ReplaceandConversion.java

---
+++ ReplaceandConversion.java/Replace.java
@@ -0,0 +1,14 @@
+class Replace{
+   public static void main(String []arge){
+
+
+   String a = "Garden";
+   String name = a.replace('a','e');
+   System.out.println(name); 
+
+
+   String b = " The Garden is Wonderfull";
+   String c = b.replace("is","was");
+   System.out.println(c); 
+}
+}
---
+++ ReplaceandConversion.java/Stringtochar.java
@@ -0,0 +1,9 @@
+class Stringtochar{
+     public static void main(String[]arge){
+     String a = "structured query language";
+     for(int i = 0; i<a.length();i++){
+     char b = a.charAt(i);
+     System.out.println(b);
+}
+}
+}
