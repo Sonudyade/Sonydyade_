9d95b28e6a5b213228adfcfd85f7cc1ced7f596e
Sonudyade <sonudyade46@gmail.com>
2022-09-07 22:01:00 +0530

    String Methods.java

---
+++ ReplaceandConversion.java/Split.java
@@ -0,0 +1,23 @@
+class Split {
+
+
+
+
+  String a = "Hello-how-are-you";
+  String[] c = a.split("-");
+
+
+
+
+ public static void main (String []arge){
+
+ Split sp = new Split();
+ for (int i=0; i<sp.c.length;i++){
+ System.out.println(sp.c[i]);
+
+ }
+
+
+}
+
+}
